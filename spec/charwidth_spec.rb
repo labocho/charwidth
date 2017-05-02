a6af43e12092ece3264b535796c09bb3f08c42e6
labocho <labocho@penguinlab.jp>
2017-05-02 11:15:00 +0900

    use rspec3 syntax

--- spec/charwidth_spec.rb
+++ spec/charwidth_spec.rb
@@ -3,42 +3,42 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Charwidth" do
   it "should convert full-width alphabet to half-width" do
-    Charwidth.normalize("ＡＢＣａｂｃ").should == "ABCabc"
+    expect(Charwidth.normalize("ＡＢＣａｂｃ")).to eq("ABCabc")
   end
   it "should convert full-width number to half-width" do
-    Charwidth.normalize("１２３").should == "123"
+    expect(Charwidth.normalize("１２３")).to eq("123")
   end
   it "should convert full-width ASCII symbol before numbers to half-width" do
-    Charwidth.normalize("！＂＃").should == "!\"#"
+    expect(Charwidth.normalize("！＂＃")).to eq("!\"#")
   end
   it "should convert full-width ASCII symbol between numbers and upper-case to half-width" do
-    Charwidth.normalize("：；").should == ":;"
+    expect(Charwidth.normalize("：；")).to eq(":;")
   end
   it "should convert full-width ASCII symbol between upper-case and lower-case to half-width" do
-    Charwidth.normalize("［＼］").should == "[\\]"
+    expect(Charwidth.normalize("［＼］")).to eq("[\\]")
   end
   it "should convert full-width ASCII symbol after lower-case to half-width" do
-    Charwidth.normalize("｛｜｝").should == "{|}"
+    expect(Charwidth.normalize("｛｜｝")).to eq("{|}")
   end
 
   it "should convert half-width CJK punctuation to full-width" do
-    Charwidth.normalize("､｡｢｣").should == "、。「」"
+    expect(Charwidth.normalize("､｡｢｣")).to eq("、。「」")
   end
 
   it "should convert half-width katakana to full-width" do
-    Charwidth.normalize("ｱｶｻﾀﾅﾊﾏﾔﾗﾜｦﾝｧｬｯｰ･").should == "アカサタナハマヤラワヲンァャッー・"
-    Charwidth.normalize("ﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉ").should == "タチツテトナニヌネノ"
+    expect(Charwidth.normalize("ｱｶｻﾀﾅﾊﾏﾔﾗﾜｦﾝｧｬｯｰ･")).to eq("アカサタナハマヤラワヲンァャッー・")
+    expect(Charwidth.normalize("ﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉ")).to eq("タチツテトナニヌネノ")
   end
 
   it "should unify half-width (semi) voiced katakana with dakuon to full-width" do
-    Charwidth.normalize("ｶﾞｻﾞﾀﾞﾊﾞﾊﾟ").should == "ガザダバパ"
+    expect(Charwidth.normalize("ｶﾞｻﾞﾀﾞﾊﾞﾊﾟ")).to eq("ガザダバパ")
   end
 
   it "should convert half-width hangul to full-width" do
-    Charwidth.normalize("ｱｶｻﾀﾅﾊﾏﾔﾗﾜｦﾝｧｬｯｰ･").should == "アカサタナハマヤラワヲンァャッー・"
+    expect(Charwidth.normalize("ｱｶｻﾀﾅﾊﾏﾔﾗﾜｦﾝｧｬｯｰ･")).to eq("アカサタナハマヤラワヲンァャッー・")
   end
 
   it "should convert IDIOGRAPHIC-SPACE to SPACE" do
-    Charwidth.normalize("\u3000").should == " "
+    expect(Charwidth.normalize("\u3000")).to eq(" ")
   end
 end
